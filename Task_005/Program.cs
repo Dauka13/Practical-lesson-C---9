c74628788c40d2b3a7d2d8d3c10e2a55aff25b75
Dauka <orazbekovdd13@mail.ru>
2022-09-10 22:18:38 +0600

    add task 005. Ackerman function

---
+++ Task_005/Program.cs
@@ -0,0 +1,21 @@
+﻿// Написать программу вычисления функции Аккермана
+
+int m = InputInt("Enter M: ");
+int n = InputInt("Enter N: ");
+Console.WriteLine($"calculation Ackerman function({m}, {n}) = {Ackerman(m, n)}");
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
+
+int Ackerman(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    if (m > 0 && n == 0)
+        return Ackerman(m - 1, 1);
+    else
+        return Ackerman(m - 1, Ackerman(m, n - 1));
+}
