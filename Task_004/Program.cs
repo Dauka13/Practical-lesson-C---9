a7e67053a064b65ae3a5e8ec3d1e54b4419c6216
Dauka <orazbekovdd13@mail.ru>
2022-09-10 22:11:28 +0600

    add task 004.Find sum of digit of the numbers

---
+++ Task_004/Program.cs
@@ -0,0 +1,13 @@
+﻿// Найти сумму цифр числа
+
+int a = int.Parse(Console.ReadLine());
+            int s = 0;
+            while (a > 0)
+            {
+                s = s + a % 10;
+                a = a /10 ;
+            }
+
+Console.Clear();
+Console.Write($"The sum of digits of the number is: ");
+Console.Write(s);
