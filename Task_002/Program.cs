3be1475fd38d903e436333e9d49b931e0e2f8909
Dauka <orazbekovdd13@mail.ru>
2022-09-10 21:51:49 +0600

    add task 002.Show natural numbers from M to N

---
+++ Task_002/Program.cs
@@ -0,0 +1,28 @@
+﻿// Показать натуральные числа от M до N, N и M заданы
+
+int InputNumber(string text)
+{
+    Console.Write(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+string StartMN(int digitM, int digitN)
+{
+    if (digitM <= digitN) return $"{digitM} " + StartMN(digitM + 1, digitN);
+    return String.Empty;
+}
+
+string FinishMN(int digitM, int digitN)
+{
+    if (digitM >= digitN) return $"{digitM} " + FinishMN(digitM - 1, digitN);
+    return String.Empty;
+}
+
+int numberM = InputNumber("Enter M: ");
+int numberN = InputNumber("Enter N: ");
+
+if (numberM <= numberN) 
+    Console.WriteLine(StartMN(numberM, numberN));
+else if (numberM > numberN)
+    Console.WriteLine(FinishMN(numberM, numberN));
