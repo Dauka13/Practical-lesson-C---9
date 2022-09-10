13a0e8ced56227d0f8cba8262c24aca27529bad0
Dauka <orazbekovdd13@mail.ru>
2022-09-10 22:00:45 +0600

    add task 003. Find sum of elements from M to N

---
+++ Task_003/Program.cs
@@ -0,0 +1,18 @@
+﻿// Найти сумму элементов от M до N, N и M заданы
+
+int m = InputInt("Enter M: ");
+int n = InputInt("Enter N: ");
+Console.WriteLine($"Sum of elements from {m} to {n} = {CountNaturalSum(m, n)}");
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
+
+int CountNaturalSum(int m, int n)
+{
+    if (m == n)
+        return n;
+    return n + CountNaturalSum(m, n - 1);
+}
