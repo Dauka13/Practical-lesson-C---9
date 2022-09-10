52fb3ba472298ca06e6d9b6ecf1b571a985d335a
Dauka <orazbekovdd13@mail.ru>
2022-09-10 22:34:31 +0600

    add task 006. number a to power b

---
+++ Task_006/Program.cs
@@ -0,0 +1,13 @@
+﻿// Написать программу возведения числа А в целую степень B
+
+Console.Clear();
+Console.Write("Enter number A: ");
+int num = int.Parse(Console.ReadLine());
+Console.Write("Enter power B: ");
+int n = int.Parse(Console.ReadLine());
+int num_n=1;
+for(int i=0; i<n; i++)
+    {
+        num_n*=num;
+    }
+Console.WriteLine("{0} ^ {1} = {2}", num, n, num_n);
