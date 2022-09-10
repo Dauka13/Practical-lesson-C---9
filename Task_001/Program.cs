9fd99e019c47925c128343af35f876b0922b955e
Dauka <orazbekovdd13@mail.ru>
2022-09-10 20:37:21 +0600

    add task 001. Natural numbers from N to 1

---
+++ Task_001/Program.cs
@@ -0,0 +1,19 @@
+﻿// Показать натуральные числа от N до 1, N задано
+
+int InputNumber(string text)
+{
+    Console.Write(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+string ShowNaturalNumbers (int digit, int n)
+{
+    if (digit >= n) return $"{digit} " + ShowNaturalNumbers(digit - 1, n); 
+    else return String.Empty;
+}
+
+int numberN = InputNumber("Enter N: ");
+int number = 1;
+
+Console.WriteLine(ShowNaturalNumbers(numberN, number));
